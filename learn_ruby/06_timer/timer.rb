4fad4d13c04c5dc6c0a0eca27cbcb45a287b493d
grzegorzzajac1989 <zajacgrzegorz1989@gmail.com>
2016-08-09 12:46:54 +0200

    Add solution 06_timer exercise

--- learn_ruby/06_timer/timer.rb
+++ learn_ruby/06_timer/timer.rb
@@ -1,3 +1,17 @@
 class Timer
-  #write your code here
+  attr_accessor :seconds
+
+
+  def initialize
+    @seconds = 0
+  end
+
+  def time_string
+    sec = @seconds % 60
+    minutes = @seconds / 60 % 60
+    hours = @seconds / 60 / 60
+
+    sprintf("%02d:%02d:%02d", hours,minutes, sec)
+  end
+
 end
