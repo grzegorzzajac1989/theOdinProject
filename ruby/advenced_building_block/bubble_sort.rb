ecb0d44ed020a880729fc04c56583f0525be090b
grzegorzzajac1989 <zajacgrzegorz1989@gmail.com>
2016-08-29 22:34:19 +0200

    add buuble_sort solution

--- ruby/advenced_building_block/bubble_sort.rb
+++ ruby/advenced_building_block/bubble_sort.rb
@@ -1,1 +1,18 @@
 #!/usr/bin/ruby
+
+def bubble_sort(array)
+  #external loop
+  for i in 1...(array.length) do
+  	#internal loop
+	for i in 1...array.length do
+	  #compare
+	  if array[i-1] > array[i]
+	  	#swap
+		array[i-1], array[i] = array[i], array[i-1]		
+	  end
+	end
+  end
+puts array
+end
+
+bubble_sort([4,3,78,2,0,2])
