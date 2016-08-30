d3ac7afb6b6a22e2f6372a47b52284b353215481
grzegorzzajac1989 <zajacgrzegorz1989@gmail.com>
2016-08-30 14:21:49 +0200

    git remote set-url origin git+ssh

--- ruby/advenced_building_block/additonal_exercise/yield.rb
+++ ruby/advenced_building_block/additonal_exercise/yield.rb
@@ -6,3 +6,6 @@ def print_list(array, first = 1)
   end
 end
 print_list(["C", "R", "C#", "Ruby"], 9) { |n| "(#{n*50})" }
+
+
+# git remote set-url origin git+ssh://git@github.com/grzegorzzajac1989/theOdinProject.git
