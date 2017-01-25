58a9562894a9a5715f106fe1a6ff38cada857734
grzegorzzajac1989 <zajacgrzegorz1989@gmail.com>
2017-01-25 12:27:00 +0100

    Add another exercise to r_on_the_net

---
+++ ruby/ruby_on_the_net/begining_ruby_Peter_Cooper/hpricot1.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/ruby
+
+require 'rubygems'
+require 'hpricot'
+require 'open-uri'
+
+doc = Hpricot(open('http://www.rubyinside.com/test.html'))
+puts doc.search("h1").first.inner_html
+
+list = doc.search("ul").first
+list.search("li").each do |item|
+	puts item.inner_html
+end
+
+list = doc.at("ul")
